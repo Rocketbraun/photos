a2160af08fba57adb69861fa0f2242e3ac5f385a
Keturah <keturah@Keturahs-MacBook-Air.local>
2024-05-20 16:08:28 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function thumb(s) {
+    document.getElementById('main').src = s;
+}  
+
+const images = document.querySelectorAll('#sidebar img');
+images.forEach(img => {
+    img.addEventListener('click', function() {
+        thumb(this.src);
+    });
+});
+
+var foo = ["foo", "bar", "baz",];
+
+console.log(foo);
+foo.forEach(e => {console.log(e)})
